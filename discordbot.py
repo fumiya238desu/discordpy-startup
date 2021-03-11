246fac728d79ef6318ad6a78a595bfa5b5d6c457
fumiya238desu <80454429+fumiya238desu@users.noreply.github.com>
2021-03-11 20:03:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,7 +23,7 @@ async def しおみ(ctx):
     
 @bot.command()
 async def にゃーん(ctx):
-file = discord.File(./discordpy-startup/shiomi/shiomi)
+file = discord.File(./shiomi/shiomi)
 await ctx.send(file=file, content='相性表')
     
 
